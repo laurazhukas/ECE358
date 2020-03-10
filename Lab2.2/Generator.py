716b4c04cbeab233c7d01009545993c5d5353b41
laurazhukas <lzhukas@hotmail.ca>
2020-03-09 23:08:01 -0400

    update plots and duration to 1000

--- Lab2.2/Generator.py
+++ Lab2.2/Generator.py
@@ -2,7 +2,7 @@ import random
 import math
 
 def generate_exponential_random_var(lam):
-    # random.seed()
+    random.seed()
     u = random.random()
     return -(1/lam)*math.log(1-u)
 
--- Lab2.2/main.py
+++ Lab2.2/main.py
@@ -12,12 +12,9 @@ def simulate_persistent_case():
     s = float((2/3)*c) # meters/sec 2*10**8
     L = 1500 # bits
     R = 1*10**6 # bps --> 1Mbps
-    t = 10 # time in ticks
+    t = 1000 # time in ticks
     n = [20, 40, 60, 80, 100] # number of nodes
     A = [7, 10, 20] # packets/second
-
-    n_test = 10 # TODO: delete
-    t_test = 100 # TODO: delete
 
     throughput_data = [[], [], []]
     efficiency_data = [[], [], []]
@@ -26,29 +23,31 @@ def simulate_persistent_case():
     for i, lam in enumerate(A):
         print("lamda: " + str(lam))
         for j , node_count in enumerate(n):
-            sim = PersistentSimulation.PersistentSimulation(lam, node_count, t_test, d, s, L, R)
+            sim = PersistentSimulation.PersistentSimulation(lam, node_count, t, d, s, L, R)
             throughput_data[i].append(sim.get_throughput())
             efficiency_data[i].append(sim.get_efficiency())
 
     ################# PLOT EFFICIENCY ################# 
-    plt.figure()
     plt.title("Efficiency vs Number of Nodes: Persistent")
     plt.xlabel('Number of Nodes')
     plt.ylabel('Efficiency')
     for i, lam in enumerate(A):
         plt.plot(n, efficiency_data[i], label='Arrival Rate: T= {}'.format(lam))
     plt.legend()
-    plt.show()
+    # plt.show()
+    plt.savefig('Persistent-Eff-Dur1000.png', bbox_inches='tight')
+    plt.close()
 
     ################# PLOT THROUGHPUT ################# 
-    plt.figure()
     plt.title("Throughput vs Number of Nodes: Persistent")
     plt.xlabel('Number of Nodes')
     plt.ylabel('Throughput (Mbps)')
     for i, lam in enumerate(A):
         plt.plot(n, throughput_data[i], label='Arrival Rate: T= {}'.format(lam))
     plt.legend()
-    plt.show()
+    # plt.show()
+    plt.savefig('Persistent-Thrput-Dur1000.png', bbox_inches='tight')
+    plt.close()
 
 def simulate_nonpersistent_case():
 
@@ -58,12 +57,9 @@ def simulate_nonpersistent_case():
     s = float((2/3)*c) # meters/sec 2*10**8
     L = 1500 # bits
     R = 1*10**6 # bps --> 1Mbps
-    t = 10 # time in ticks
+    t = 1000 # time in ticks
     n = [20, 40, 60, 80, 100] # number of nodes
     A = [7, 10, 20] # packets/second
-
-    n_test = 10 # TODO: delete
-    t_test = 100 # TODO: delete
 
     throughput_data = [[], [], []]
     efficiency_data = [[], [], []]
@@ -72,33 +68,35 @@ def simulate_nonpersistent_case():
     for i, lam in enumerate(A):
         print("lamda: " + str(lam))
         for j , node_count in enumerate(n):
-            sim = NonpersistentSimulation.NonpersistentSimulation(lam, node_count, t_test, d, s, L, R)
+            sim = NonpersistentSimulation.NonpersistentSimulation(lam, node_count, t, d, s, L, R)
             throughput_data[i].append(sim.get_throughput())
             efficiency_data[i].append(sim.get_efficiency())
 
     ################# PLOT EFFICIENCY ################# 
-    plt.figure()
     plt.title("Efficiency vs Number of Nodes: Non-Persistent")
     plt.xlabel('Number of Nodes')
     plt.ylabel('Efficiency')
     for i, lam in enumerate(A):
         plt.plot(n, efficiency_data[i], label='Arrival Rate: T= {}'.format(lam))
     plt.legend()
-    plt.show()
+    # plt.show()
+    plt.savefig('Non-Persistent-Eff-Dur1000.png', bbox_inches='tight')
+    plt.close()
 
     ################# PLOT THROUGHPUT ################# 
-    plt.figure()
     plt.title("Throughput vs Number of Nodes: Non-Persistent")
     plt.xlabel('Number of Nodes')
     plt.ylabel('Throughput (Mbps)')
     for i, lam in enumerate(A):
         plt.plot(n, throughput_data[i], label='Arrival Rate: T= {}'.format(lam))
     plt.legend()
-    plt.show()
+    # plt.show()
+    plt.savefig('Non-Persistent-Thrput-Dur1000.png', bbox_inches='tight')
+    plt.close()
 
 def main():
     print("Persistent Case")
-    # simulate_persistent_case()
+    simulate_persistent_case()
 
     print("Non-Persistent Case")
     simulate_nonpersistent_case()
