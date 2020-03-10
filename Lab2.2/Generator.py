82b6e3bffa77727ce970b518f1d8dd657059afc7
CaitlinOCallaghan <caitlinocallaghan22@gmail.com>
2020-03-10 16:24:38 -0400

    clean-up gnerator

--- Lab2.2/Generator.py
+++ Lab2.2/Generator.py
@@ -2,11 +2,11 @@ import random
 import math
 
 def generate_exponential_random_var(lam):
-    # random.seed()
+    random.seed()
     u = random.random()
     return -(1/lam)*math.log(1-u)
 
 def generate_exp_backoff(backoff_counter):
-    # random.seed() # TODO: do we need this...?
-    r = random.randrange(0,(2**backoff_counter), 1)  # TODO: verify this is correct NOTE: no -1 because non-inclusive
-    return r * (512/10**6)
+    random.seed()
+    r = random.randrange(0,(2**backoff_counter), 1)  # NOTE: do not subtract "1" because of non-inclusive bounds
+    return r * (512/10**6)  # 512 is the bit time, 10**6 for scaling
